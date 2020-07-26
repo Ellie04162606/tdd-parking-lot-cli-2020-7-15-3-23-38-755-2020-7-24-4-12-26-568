55bbe089690b839fb564ed95d3316dee71f56fd3
Ellie04162606 <1181140808@qq.com>
2020-07-26 16:57:32 +0800

    [ParkingBoyFacts_Story_4][Ellie] feature:should_return_no_car_when_smart_parking_boy_fetching_given_without_ticket

--- src/test/java/com/oocl/cultivation/test/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/test/ParkingBoyFacts.java
@@ -334,6 +334,22 @@ class ParkingBoyFacts {
     }
 
     @Test
+    void should_return_no_car_when_smart_parking_boy_fetching_given_without_ticket() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot("parkingLot" + i));
+        }
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        //when
+        Car car = smartParkingBoy.fetching(null);
+
+        //then
+        assertNull(car);
+    }
+
+    @Test
     void should_return_ticket_with_car_parking_in_parking_lot_which_has_a_larger_available_position_rate_given_a_car_and_a_super_smart_parking_boy() {
         //given
         List<ParkingLot> parkingLots = new ArrayList<>();
